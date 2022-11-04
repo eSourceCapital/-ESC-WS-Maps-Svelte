5550ba17c964bf661f0dbdc37baf64c61249bff2
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2022-11-04 16:54:20 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+  import svelteLogo from "./assets/svelte.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Map from "./Map.svelte";
+  export let ready;
+</script>
+
+<svelte:head>
+  <script
+    defer
+    async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC5Zp98lbWecDHTKT1gSCyAv595X-dSyKk&callback=initMap"
+  >
+  </script>
+</svelte:head>
+
+<main>
+  <h1>Google Maps + Svelte</h1>
+
+  {#if ready}
+    <Map /><div class="card">
+      <Counter />
+    </div>
+  {/if}
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
+
+<style>
+  :global(body) {
+    padding: 0;
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
---
+++ src/Map.svelte
@@ -0,0 +1,39 @@
+<script>
+  // import mapStyles from './map-styles'; // optional
+
+  let container;
+  let map;
+  let marker;
+  let zoom = 8;
+  let center = { lat: -34.397, lng: 150.644 };
+
+  import { onMount } from "svelte";
+
+  function placeMarker(location) {
+    let marker = new google.maps.Marker({
+      position: location,
+      map: map,
+    });
+  }
+
+  onMount(async () => {
+    map = new google.maps.Map(container, {
+      zoom,
+      center,
+      //    styles: mapStyles // optional
+    });
+    google.maps.event.addListener(map, "click", function (event) {
+      placeMarker(event.latLng);
+    });
+    placeMarker(center);
+  });
+</script>
+
+<div class="full-screen" bind:this={container} />
+
+<style>
+  .full-screen {
+    width: 50vw;
+    height: 50vh;
+  }
+</style>
